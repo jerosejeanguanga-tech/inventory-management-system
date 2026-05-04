28cbfd1df6d664892dc77d2a7bbc9fe51e5e7475
jerosejeanguanga-tech <jerosejeanguanga@gmail.com>
2026-05-04 18:55:18 +0800

    fix: show navbar on all user pages, hide only on admin routes

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
 import { FooterComponent } from './components/footer/footer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: `
-    <app-navbar *ngIf="!isAdminRoute()"></app-navbar>
-    <main class="min-h-screen bg-gray-50">
-      <router-outlet></router-outlet>
-    </main>
-    <app-footer></app-footer>
-  `
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  currentUrl = '';
+
   constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe((e: any) => {
+      this.currentUrl = e.url;
+    });
+    this.currentUrl = this.router.url;
+  }
+
   isAdminRoute(): boolean {
-    return this.router.url.startsWith('/admin');
+    return this.currentUrl.startsWith('/admin');
   }
 }
